477ee82da5b65bfe61ab3375b36e19d59f353b0a
Abdulaziz Nazirov <91299565+nazirov-dev@users.noreply.github.com>
2022-07-29 14:13:09 +0500

    Update namoz.php

--- namoz.php
+++ namoz.php
@@ -1,6 +1,7 @@
 <?php
 
-class Namoz{
+class Namoz
+{
     private string $city;
     private string $type;
 
@@ -13,40 +14,41 @@ class Namoz{
     public function get(): array
     {
         $citys = [
-            'toshkent'=>27,
-            'andijon'=>1,
-            'buxoro'=>4,
-            'guliston'=>5,
-            'samarqand'=>18,
-            'namangan'=>15,
-            'navoiy'=>14,
-            'jizzax'=>9,
-            'nukus'=>16,
-            'qarshi'=>25,
-            'qoqon'=>26,
-            'xiva'=>21,
-            'margilon'=>13
+            'toshkent' => 27,
+            'andijon' => 1,
+            'buxoro' => 4,
+            'guliston' => 5,
+            'samarqand' => 18,
+            'namangan' => 15,
+            'navoiy' => 14,
+            'jizzax' => 9,
+            'nukus' => 16,
+            'qarshi' => 25,
+            'qoqon' => 26,
+            'xiva' => 21,
+            'margilon' => 13
         ]; //Qabul qilinadigan shaharlar
 
-        if(!isset($citys[strtolower($_GET['city'])])){
-            echo json_encode(['status'=>false, 'result'=>"Kiritilgan hudud topilmadi!"], JSON_PRETTY_PRINT);
+        
+        if (!isset($citys[strtolower($_GET['city'])])) {
+            echo json_encode(['status' => false, 'result' => "Kiritilgan hudud topilmadi!"], JSON_PRETTY_PRINT);
             exit;
         }
         $oy = date("m");
-        if($oy[0] == "0") $oy = str_replace("0", "", $oy);
+        if ($oy[0] == "0") $oy = str_replace("0", "", $oy);
 
-        $get = file_get_contents("https://islom.uz/vaqtlar/".$citys[str_replace("'", "", strtolower($_GET['city']))]."/".$oy);
+        $get = file_get_contents("https://islom.uz/vaqtlar/" . $citys[str_replace("'", "", strtolower($_GET['city']))] . "/" . $oy);
 
         $response = [
-            'status'=> true,
-            'dev'=>"Abdulaziz Nazirov (@Nazirov_Dev)"
+            'status' => true,
+            'dev' => "Abdulaziz Nazirov (@Nazirov_Dev)"
         ];
 
-        if(mb_stripos($get, "</tr><tr class='p_day bugun'>")!==false)
+        if (mb_stripos($get, "</tr><tr class='p_day bugun'>") !== false)
             $array = explode("</tr><tr class='p_day bugun'>", $get)[1];
-        elseif(mb_stripos($get, "</tr><tr class='juma bugun'>")!==false)
+        elseif (mb_stripos($get, "</tr><tr class='juma bugun'>") !== false)
             $array = explode("</tr><tr class='juma bugun'>", $get)[1];
-        else{
+        else {
             $response['status'] = false;
             $response['result'] = "Ma'lumot topilmadi";
             echo json_encode($response, JSON_PRETTY_PRINT);
@@ -60,18 +62,14 @@ class Namoz{
         $shom = trim($array[$_GET['type'] == "bugungi" ? 8 : 18]);
         $xufton = trim($array[$_GET['type'] == "bugungi" ? 9 : 19]);
         $response['result'] = [
-            'sahar'=>$sahar,
-            'quyosh'=>$quyosh,
-            'peshin'=>$peshin,
-            'asr'=>$asr,
-            'shom'=>$shom,
-            'xufton'=>$xufton
+            'sahar' => $sahar,
+            'quyosh' => $quyosh,
+            'peshin' => $peshin,
+            'asr' => $asr,
+            'shom' => $shom,
+            'xufton' => $xufton
         ];
 
-        echo $response;
-
+        return $response;
     }
 }
-
-
-
